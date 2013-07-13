8868367894868bbc0bcd57991e3d49262fb533e6
drpepper <jesse@heavenstone.net>
2013-07-13 16:49:57 +0200

    started match.js

---
+++ match.js
@@ -0,0 +1,36 @@
+var LIFE_POINTS = 5;
+
+
+// Left and right are player structures, like { defense: 5, attack: 4, reproduction: 9 }
+// Returns an object like { winner: "left", rounds: [ { attacker: "right", "winner": "left" }, ... ] }
+function battle(left, right) {
+  var players = { left: left, right: right };
+  var lifePoints = { left: LIFE_POINTS, right: LIFE_POINTS };
+  var history = [];
+  var attacker = Math.random() > 0.5 ? "left" : "right";
+  while(lifePoints.left > 0 && lifePoints.right > 0) {
+    var attackValue = players[attacker].attack * Math.random();
+    var defenseValue = players[complement(attacker)].defense * Math.random();
+    var round = { attacker: attacker };
+
+    if(attackValue > defenseValue) {
+      lifePoints[complement(attacker)]--;
+      round.winner = attacker;
+    } else {
+      round.winner = complement(attacker);
+    }
+
+    history.push(rounds);
+
+    attacker = complement(attacker);
+  }
+  var winner = lifePoints.left > 0 ? "left" : "right";
+  return { winner: winner, history: history };
+}
+
+// Returns an object representing fitness, like { attack: 3, defense: 4, reproduction: 7 }
+function fitness(championGenes) {
+
+}
+
+function complement(leftOrRight) { return leftOrRight === "left" ? "right" : "left" }
